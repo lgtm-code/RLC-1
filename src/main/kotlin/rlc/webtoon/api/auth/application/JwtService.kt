e57d4eec1542b5c46c760a936c57794a7bf903f5
crinkj <jcrinkey89120@gmail.com>
2024-02-16 10:20:19 +0900

    feat: jwt서비스 작성

---
+++ src/main/kotlin/rlc/webtoon/api/auth/application/JwtService.kt
@@ -0,0 +1,55 @@
+package rlc.webtoon.api.auth.application
+
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.security.Keys
+import org.springframework.stereotype.Service
+import io.jsonwebtoken.Jwts
+import rlc.webtoon.api.config.JwtProperties
+import java.util.Date
+import javax.crypto.SecretKey
+
+@Service
+class JwtService(
+        jwtProperties: JwtProperties
+) {
+    private val secretKey: SecretKey = Keys.hmacShaKeyFor(
+            jwtProperties.key.toByteArray()
+    )
+
+    fun generate(userId: String,
+                 expiration: Date,
+                 additionalClaims: Map<String, Any> = emptyMap()
+    ): String =
+            Jwts.builder()
+                    .claims()
+                    .issuedAt(Date(System.currentTimeMillis()))
+                    .expiration(expiration)
+                    .add(additionalClaims)
+                    .and()
+                    .signWith(secretKey)
+                    .compact()
+
+    fun isValid(jwt: String, userId: String): Boolean {
+        val extractedUserId: String = extractUserId(jwt)
+        return userId == extractedUserId && !isExpired(jwt)
+    }
+
+    fun extractUserId(jwt: String): String =
+            getAllClaims(jwt)
+                    .subject
+
+    private fun isExpired(jwt: String): Boolean =
+            getAllClaims(jwt)
+                    .expiration
+                    .before(Date(System.currentTimeMillis()))
+
+    private fun getAllClaims(jwt: String): Claims {
+        val parser = Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+        return parser
+                .parseSignedClaims(jwt)
+                .payload
+    }
+
+}
